14010303a7cbdfd9dbe85c09d64d30c22443a70a
Stu Bolderson <stu.bolderson@aol.com>
2020-12-07 11:39:32 +0100

    aoout us section html added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import React from 'react';
+// pages
+import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello World</h1>
+      <AboutUs />
     </div>
   );
 }
---
+++ src/components/AboutSection.js
@@ -0,0 +1,35 @@
+import React from 'react';
+// images
+import home1 from '../img/home1.png';
+
+const AboutSection = () => {
+  return (
+    <div>
+      <div className='description'>
+        <div className='title'>
+          <div className='hide'>
+            <h2>We work to make</h2>
+          </div>
+          <div className='hide'>
+            <h2>
+              your <span>dreams</span> come
+            </h2>
+          </div>
+          <div className='hide'>
+            <h2>true</h2>
+          </div>
+        </div>
+        <p>
+          Contact us for any photography or videogaphy ideas that you have. We
+          have expert professionals ready to hear from you !
+        </p>
+        <button>Contact Us</button>
+      </div>
+      <div className='image'>
+        <img src={home1} alt='home 1' />
+      </div>
+    </div>
+  );
+};
+
+export default AboutSection;
